4f034650677f8f564570c0950e2227d1d64f68f0
hyhy987 <sie.haoyang@gmail.com>
2025-08-20 14:39:32 +0800

    Renaming and changing of greet message

---
+++ src/main/java/LilBird.java
@@ -0,0 +1,11 @@
+public class Duke {
+    public static void main(String[] args) {
+        String skeleton = "____________________________________________________________\n" +
+                " Hello! I'm Lil Bird\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
+        System.out.println(skeleton);
+    }
+}
