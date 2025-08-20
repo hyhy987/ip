74e277460670b707a014575bd0e2619345fdb3b0
hyhy987 <sie.haoyang@gmail.com>
2025-08-20 16:58:05 +0800

    Added list command

--- src/main/java/LilBird.java
+++ src/main/java/LilBird.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class LilBird {
     private static final String LINE = "____________________________________________________________";
-
     private static void printBox(String message) {
         System.out.println("    " + LINE);
         //to support multi-line messages
@@ -14,12 +13,31 @@ public class LilBird {
 
     public static void main(String[] args) {
         printBox("Hello! I'm LilBird\nWhat can I do for you?");
+
+        //Store and track the tasks
+        private String[] tasks = new String[100];
+        int count = 0;
+
         Scanner sc = new Scanner(System.in);
+
         while (true) {
-            String input = sc.nextLine();
+            if (!sc.hasNextLine()) break;
+            String input = sc.nextLine().trim();
+
             if (input.equals("bye")) {
                 printBox("Bye. Hope to see you again soon!");
                 break;
+            } else if (input.equals("list")) {
+                if (count == 0) {
+                    printBox("");
+                } else {
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0; i < count; i++) {
+                        sb.append(i+1).append(". ").append(tasks[i]);
+                        if (i < count - 1) sb.append("\n");
+                    }
+                    printBox(sb.toString());
+                }
             }
             //Echo whatever user types
             printBox(input);
