a2e203ff5be21e19de39e699617c35442cb57062
hyhy987 <sie.haoyang@gmail.com>
2025-08-20 15:10:09 +0800

    Added Echo Feature

--- src/main/java/LilBird.java
+++ src/main/java/LilBird.java
@@ -1,11 +1,29 @@
-public class Duke {
+import java.util.Scanner;
+
+public class LilBird {
+    private static final String LINE = "____________________________________________________________";
+
+    private static void printBox(String message) {
+        System.out.println("    " + LINE);
+        //to support multi-line messages
+        for (String line : message.split("\\R")) {
+            System.out.println("    " + line);
+        }
+        System.out.println("    " + LINE);
+    }
+
     public static void main(String[] args) {
-        String skeleton = "____________________________________________________________\n" +
-                " Hello! I'm Lil Bird\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n";
-        System.out.println(skeleton);
+        printBox("Hello! I'm LilBird\nWhat can I do for you?");
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                printBox("Bye. Hope to see you again soon!");
+                break;
+            }
+            //Echo whatever user types
+            printBox(input);
+        }
+        sc.close();
     }
 }
